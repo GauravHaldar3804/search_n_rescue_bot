2572a7b6c6ce57cfbd2fd3d4daad5c2c4764c69c
Aaditi04 <aadidkadam415@gmail.com>
2025-03-31 09:59:00 +0530

    Remove build, install, and log folders from repo

--- build/sensor_gui/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aaditi/ros2_ws/src/sensor_gui
-# Build directory: /home/aaditi/ros2_ws/build/sensor_gui
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
