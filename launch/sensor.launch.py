da0732d6fb9111821027aaa7881a43872741fd9a
Gaurav Bansi Haldar <haldargaurav04@gmail.com>
2025-04-04 02:49:46 +0530

    Fixed the paths for launch file

--- launch/sensor.launch.py
+++ launch/sensor.launch.py
@@ -1,30 +1,28 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-import os
 
 def generate_launch_description():
     package_name = 'main_package'
-    package_path = os.path.join('/home/gaurav/searchnrescue_dev_ws/install/', package_name, 'lib', package_name, 'nodes')
 
     return LaunchDescription([
         Node(
             package=package_name, 
-            executable=os.path.join(package_path, 'ultrasonic_node.py'), 
+            executable='ultrasonic_node.py',  # No absolute paths!
             output='screen'
         ),
         Node(
             package=package_name, 
-            executable=os.path.join(package_path, 'camera_node.py'), 
+            executable='camera_node.py', 
             output='screen'
         ),
         Node(
             package=package_name, 
-            executable=os.path.join(package_path, 'gps_node.py'), 
+            executable='gps_node.py', 
             output='screen'
         ),
         Node(
             package=package_name, 
-            executable=os.path.join(package_path, 'imu_node.py'), 
+            executable='imu_node.py', 
             output='screen'
         ),
     ])
