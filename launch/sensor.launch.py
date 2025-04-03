6e7442dfd3db4e9936660b1b6fca10b8642d368a
Gaurav Bansi Haldar <haldargaurav04@gmail.com>
2025-04-04 02:24:28 +0530

    launch file

--- launch/sensor.launch.py
+++ launch/sensor.launch.py
@@ -1,10 +1,30 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
 
 def generate_launch_description():
+    package_name = 'main_package'
+    package_path = os.path.join('/home/gaurav/searchnrescue_dev_ws/install/', package_name, 'lib', package_name, 'nodes')
+
     return LaunchDescription([
-        Node(package='main_package', executable='ultrasonic_node.py', output='screen'),
-        Node(package='main_package', executable='camera_node.py', output='screen'),
-        Node(package='main_package', executable='gps_node.py', output='screen'),
-        Node(package='main_package', executable='imu_node.py', output='screen'),
+        Node(
+            package=package_name, 
+            executable=os.path.join(package_path, 'ultrasonic_node.py'), 
+            output='screen'
+        ),
+        Node(
+            package=package_name, 
+            executable=os.path.join(package_path, 'camera_node.py'), 
+            output='screen'
+        ),
+        Node(
+            package=package_name, 
+            executable=os.path.join(package_path, 'gps_node.py'), 
+            output='screen'
+        ),
+        Node(
+            package=package_name, 
+            executable=os.path.join(package_path, 'imu_node.py'), 
+            output='screen'
+        ),
     ])
